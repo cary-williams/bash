4f6eacdacdd08761d9d84f297226f3e9cc47bfb7
Cary Williams <cary@linux.com>
2021-10-01 11:51:48 -0500

    Update help-template.sh

--- help-template.sh
+++ help-template.sh
@@ -34,3 +34,13 @@ Help()
    echo ""
 }
 
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+     \?) # incorrect option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
